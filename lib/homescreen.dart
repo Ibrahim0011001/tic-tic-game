e75be1ee0b217be611b4c8599d6064998889d329
Ibrahim0011001 <hymarhym@gmail.com>
2024-07-21 00:22:29 +0300

    first commit

--- lib/homescreen.dart
+++ lib/homescreen.dart
@@ -1,12 +1,34 @@
 import 'package:flutter/material.dart';
 
-class Homescreen extends StatelessWidget {
+import 'game_logic.dart';
+
+class Homescreen extends StatefulWidget {
   const Homescreen({super.key});
 
   @override
+  State<Homescreen> createState() => _HomescreenState();
+}
+
+class _HomescreenState extends State<Homescreen> {
+  String activeplayer = 'X';
+  bool gameOver = false;
+  int turn = 0;
+  String result = '';
+  Game game = Game();
+  bool isswitched = false;
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      body: SafeArea(
+        child: Column(
+          children: [
+            SwitchListTile.adaptive(
+              value: isswitched,
+              onChanged: (value) {},
+            )
+          ],
+        ),
+      ),
     );
   }
 }
